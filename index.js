43170d600740c020cd9d5f9ff42962da73890cb9
matheusmaais <matheusmaais@gmail.com>
2020-07-06 14:49:25 -0300

    testando

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const PORT = 3000;
+const HOST = '0.0.0.0';
+const app = express ();
+app.get ('/', (req, res) => {
+    res.send('Hello World23');
+});
+
+app.listen(PORT, HOST);
