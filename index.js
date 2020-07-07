bef2c2647fd4f0a5bd32b483d3741b9004c06bc4
matheusmaais <matheusmaais@gmail.com>
2020-07-07 12:57:21 -0300

    testesadasd

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express ();
 app.get ('/', (req, res) => {
-<<<<<<< HEAD
-    res.send('novoBuilddenovoo');
-=======
-    res.send('novoBuild222');
->>>>>>> deaeccfee1df2b50c4b0bdda0c4584b299bf2868
+    res.send('novoBuilddenovoo')
+
 });
 
 app.listen(PORT, HOST);
