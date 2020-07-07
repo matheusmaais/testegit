deaeccfee1df2b50c4b0bdda0c4584b299bf2868
matheusmaais <66253456+matheusmaais@users.noreply.github.com>
2020-07-07 11:05:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express ();
 app.get ('/', (req, res) => {
-    res.send('novoBuild');
+    res.send('novoBuild222');
 });
 
 app.listen(PORT, HOST);
